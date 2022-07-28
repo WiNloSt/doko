9817c85711b4eec97055695b5fdab118a536ae7b
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2022-07-28 22:14:40 +0700

    tcring

---
+++ kelvin-o/pokerhand/pokerhand.go
@@ -0,0 +1,24 @@
+package pokerhand
+
+import "strings"
+
+func runGames(games string) float64 {
+	if len(games) == 0 {
+		return 0.0
+	}
+
+	winCount := 0.0
+	gameCount := 0.0
+	for _, game := range strings.Split(games, "\n") {
+		gameCount += 1
+		if player1Win(game) {
+			winCount += 1
+		}
+	}
+
+	return winCount / gameCount
+}
+
+func player1Win(game string) bool {
+	return game[12] == '8'
+}
--- kelvin-o/pokerhand/pokerhand_test.go
+++ kelvin-o/pokerhand/pokerhand_test.go
@@ -11,41 +11,24 @@ const p2WinByHighCard = "2C 3S 4C 5H 7S 2D 3S 4D 5S 8C"
 
 func TestPokerHand(t *testing.T) {
 	t.Run("it should win 0 with no game", func(t *testing.T) {
-		assert.Equal(t, runGames(""), 0.0)
+		assert.Equal(t, 0.0, runGames(""))
 	})
 
 	t.Run("it should win 1 with high A", func(t *testing.T) {
-		assert.Equal(t, runGames(p1WinByHighCard), 1.0)
+		assert.Equal(t, 1.0, runGames(p1WinByHighCard))
 	})
 
 	t.Run("it should win 0 with P2 high A", func(t *testing.T) {
-		assert.Equal(t, runGames(p2WinByHighCard), 0.0)
+		assert.Equal(t, 0.0, runGames(p2WinByHighCard))
 	})
 
 	t.Run("player1 win twice", func(t *testing.T) {
 		games := p1WinByHighCard + "\n" + p1WinByHighCard
-		assert.Equal(t, runGames(games), 1.0)
+		assert.Equal(t, 1.0, runGames(games))
 	})
 
 	t.Run("player1 win once player2 win once", func(t *testing.T) {
 		games := p1WinByHighCard + "\n" + p2WinByHighCard
-		assert.Equal(t, runGames(games), 1.0)
+		assert.Equal(t, 0.5, runGames(games))
 	})
-
-}
-
-func runGames(games string) float64 {
-	if len(games) == 0 {
-		return 0.0
-	}
-
-	if player1Win(games) {
-		return 1.0
-	}
-
-	return 0.0
-}
-
-func player1Win(game string) bool {
-	return game[12] == '8'
 }
