0085841ce136fd829e94321ecf2e9515ae13936b
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2022-07-28 22:47:46 +0700

    tcring

--- kelvin-o/pokerhand/pokerhand.go
+++ kelvin-o/pokerhand/pokerhand.go
@@ -20,15 +20,19 @@ func runGames(games string) float64 {
 }
 
 func player1Win(game string) bool {
-	p1Hand := game[0:14]
-	p2Hand := game[15:]
-	return getRank(getHandCardRank(p1Hand, 4)) > getRank(getHandCardRank(p2Hand, 4))
+	p1Hand := Hand{cards: game[0:14]}
+	p2Hand := Hand{cards: game[15:]}
+	return getRank(p1Hand.getCardRank(4)) > getRank(p2Hand.getCardRank(4))
 }
 
 func getRank(cardRank byte) int {
 	return strings.Index("23456789TJQKA", string(cardRank))
 }
 
-func getHandCardRank(hand string, cardIndex int) byte {
-	return hand[cardIndex*3]
+type Hand struct {
+	cards string
+}
+
+func (hand *Hand) getCardRank(cardIndex int) byte {
+	return hand.cards[cardIndex*3]
 }
