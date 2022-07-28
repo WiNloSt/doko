dcf322a6f2427065c708d7c826f3dbe5b59d7da1
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2022-07-28 23:44:18 +0700

    tcring

--- kelvin-o/pokerhand/pokerhand.go
+++ kelvin-o/pokerhand/pokerhand.go
@@ -22,37 +22,33 @@ func runGames(games string) float64 {
 func player1Win(game string) bool {
 	p1Hand := Hand{cards: game[0:14]}
 	p2Hand := Hand{cards: game[15:]}
-	p1Hand.sortCardOneTime()
-	return p1Hand.getCardRank(4) > p2Hand.getCardRank(4)
-}
-
-func (hand *Hand) sortCardOneTime() {
-	for index := range hand.getCards() {
-		if index == 4 {
-			continue
-		}
-		if hand.getCardRank(index) > hand.getCardRank(index+1) {
-			hand.moveCard(index, index+1)
-		}
-	}
-}
-
-func (hand *Hand) moveCard(fromIndex int, toIndex int) {
-	hand.cards += " "
-	hand.cards = hand.cards[0*3:fromIndex*3] + hand.cards[(fromIndex+1)*3:(toIndex+1)*3] + hand.cards[fromIndex*3:(fromIndex+1)*3] + hand.cards[(toIndex+1)*3:]
-	hand.cards = hand.cards[0:14]
-}
-
-func (hand *Hand) getRank(cardRank byte) int {
-	return strings.Index("23456789TJQKA", string(cardRank))
+	p1Hand.SortCardOneTime()
+	return p1Hand.GetCardRank(4) > p2Hand.GetCardRank(4)
 }
 
 type Hand struct {
 	cards string
 }
 
-func (hand *Hand) getCardRank(cardIndex int) int {
-	return hand.getRank(hand.getCards()[cardIndex][0])
+func (hand *Hand) SortCardOneTime() {
+	for index := range hand.getCards() {
+		if index == 4 {
+			continue
+		}
+		if hand.GetCardRank(index) > hand.GetCardRank(index+1) {
+			hand.moveCard(index, index+1)
+		}
+	}
+}
+
+func (hand *Hand) GetCardRank(cardIndex int) int {
+	return strings.Index("23456789TJQKA", string(hand.getCards()[cardIndex][0]))
+}
+
+func (hand *Hand) moveCard(fromIndex int, toIndex int) {
+	hand.cards += " "
+	hand.cards = hand.cards[0*3:fromIndex*3] + hand.cards[(fromIndex+1)*3:(toIndex+1)*3] + hand.cards[fromIndex*3:(fromIndex+1)*3] + hand.cards[(toIndex+1)*3:]
+	hand.cards = hand.cards[0:14]
 }
 
 func (hand *Hand) getCards() []string {
