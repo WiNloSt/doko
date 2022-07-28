2cc1213c27bf9401fc5fdcf5e5701b239cc23ab8
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2022-07-28 22:24:06 +0700

    tcring

--- kelvin-o/pokerhand/pokerhand_test.go
+++ kelvin-o/pokerhand/pokerhand_test.go
@@ -6,8 +6,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-const p1WinByHighCard = "2C 3S 4C 5H 8S 2D 3S 4D 5S 7C"
-const player1WinsByHighCard9 = "2C 3S 4C 5H 9S 2D 3S 4D 5S 7C"
 const p2WinByHighCard = "2C 3S 4C 5H 7S 2D 3S 4D 5S 8C"
 
 func TestPokerHand(t *testing.T) {
@@ -15,27 +13,31 @@ func TestPokerHand(t *testing.T) {
 		assert.Equal(t, 0.0, runGames(""))
 	})
 
-	t.Run("it should win 1 with high A", func(t *testing.T) {
-		assert.Equal(t, 1.0, runGames(p1WinByHighCard))
+	t.Run("it should win 1 with high 8", func(t *testing.T) {
+		assert.Equal(t, 1.0, runGames(winsByHighestCardWith("8s")))
 	})
 
-	t.Run("it should win 0 with P2 high A", func(t *testing.T) {
+	t.Run("it should win 0 with P2 high 8", func(t *testing.T) {
 		assert.Equal(t, 0.0, runGames(p2WinByHighCard))
 	})
 
 	t.Run("player1 win twice", func(t *testing.T) {
-		games := p1WinByHighCard + "\n" + p1WinByHighCard
+		games := winsByHighestCardWith("8s") + "\n" + winsByHighestCardWith("8s")
 		assert.Equal(t, 1.0, runGames(games))
 	})
 
 	t.Run("player1 win once player2 win once", func(t *testing.T) {
-		games := p1WinByHighCard + "\n" + p2WinByHighCard
+		games := winsByHighestCardWith("8s") + "\n" + p2WinByHighCard
 		assert.Equal(t, 0.5, runGames(games))
 	})
 }
 
 func TestPlayer1Win(t *testing.T) {
 	t.Run("player 1 wins by 9 high", func(t *testing.T) {
-		assert.Equal(t, true, player1Win(player1WinsByHighCard9))
+		assert.Equal(t, true, player1Win(winsByHighestCardWith("9H")))
 	})
+}
+
+func winsByHighestCardWith(card string) string {
+	return "2C 3S 4C 5H " + card + " 2D 3S 4D 5S 7C"
 }
